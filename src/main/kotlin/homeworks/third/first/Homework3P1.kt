27cf83391f1ce048428e1514c47330f21f857c6f
Nikita Ksenchik <nksenchik@solanteq.com>
2023-10-15 16:16:08 +0300

    Written first version of 3.1

--- src/main/kotlin/homeworks/third/first/Homework3P1.kt
+++ src/main/kotlin/homeworks/third/first/Homework3P1.kt
@@ -1,5 +1,35 @@
 package homeworks.third.first
 
+import homeworks.first.Homework1
+import homeworks.second.Homework2
+import homeworks.utils.vo.Seq
+
 class Homework3P1 {
-    private val kek = null
+    private var homework2 = Homework2()
+
+    fun evalByFirstMethod(point: Double): Double {
+        val seq = Seq(-3.0, 3.0)
+        val valuesNumber = 30
+        homework2 = Homework2(
+            table = Homework2.createTable(Homework2.function, seq, valuesNumber)
+                .map { (it.value to it.key) }.toMap().toSortedMap(),
+            seq = seq,
+            valuesNumber = valuesNumber,
+            degree = 29
+        )
+
+        return homework2.evaluateByMethod("Lagrange", point)
+    }
+
+    fun evalBySecondMethod(point: Double): Double {
+        val homework1 = Homework1(
+            func = { x: Double -> homework2.function(x) - point },
+            seq = Seq(-2.0, 2.0),
+            epsilon = 1.0e-9,
+            firstDerivative = { x -> x },
+            secondDerivative = { x -> x }
+        )
+
+        return homework1.findSolutionByMethod("Secant", Seq(-2.0, 2.0)).result
+    }
 }
--- src/main/kotlin/homeworks/third/first/UiH3P1.kt
+++ src/main/kotlin/homeworks/third/first/UiH3P1.kt
@@ -3,10 +3,10 @@ package homeworks.third.first
 import homeworks.utils.ui.Ui
 
 class UiH3P1 : Ui{
-    private var homework = null
+    private var homework = Homework3P1()
 
     private fun setHomework() {
-        TODO()
+        homework = Homework3P1()
     }
 
     private fun printTaskInfo() {
@@ -18,7 +18,8 @@ class UiH3P1 : Ui{
     private fun printCommandList() {
         println("Commands list:")
         println("help                            -print command list")
-        println("back                            -back ")
+        println("back                            -quit the current task")
+        println("evalAll <point>                 -evaluate by all methods")
         println()
     }
 
@@ -56,6 +57,17 @@ class UiH3P1 : Ui{
                         }
 
                         break
+                    }
+                    "evalAll" -> {
+                        if (input.size != 2) {
+                            printErr()
+                            continue
+                        }
+
+                        val point = input[1].toDouble()
+
+                        println(homework.evalByFirstMethod(point))
+                        println(homework.evalBySecondMethod(point))
                     }
                     else -> {
                         printErr()
